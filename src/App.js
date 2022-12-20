182f962361e84dee8e176bee29f808114425a184
Lộc <locpham050100@gmail.com>
2022-12-20 18:00:17 +0700

    Navbar aka Sidebar with React Pro Sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 
 import { ColorModeContext, useMode } from "./theme";
 import Topbar from "./scenes/global/Topbar";
-import Dashboard from "./scenes/global/Dashboard";
 import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
 // import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
 // import Contacts from "./scenes/contacts";
--- src/scenes/dashbroad/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Dashbroad = () => {
-  return (
-    <div>Dashbroad</div>
-  )
-}
-
-export default Dashbroad
